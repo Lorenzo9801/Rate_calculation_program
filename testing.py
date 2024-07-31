e76cc1fb65c5f1262e4c7ecf80d7a8406e78aae5
Lorenzo <chiaralucelorenzo@alice.it>
2024-07-31 18:20:07 +0200

    Add tests for fitting parameters of cross_section and stopping_power

--- testing.py
+++ testing.py
@@ -1,81 +1,36 @@
 
-
-import functions_definition
+from functions_definition import cross_section, stopping_power, Fitting
 import numpy as np
 import pytest
-import hypothesis
-from hypothesis import strategies as st
-from hypothesis import settings
-from hypothesis import given
-import pandas as pd
-import os
-import configparser
 
 
 
-# test if with such initial values for the cross section function I obtain a positive value
-@given(x=st.just(10),xc=st.just(10),A=st.just(1),sigma=st.just(1),tau=st.just(1))
-def test_cross_section(x,xc,A,sigma,tau):
-    model= functions_definition.cross_section(x,xc,A,sigma,tau)
-    assert model>0
+def test_fitting_cs_params_positive():
 
-# test if with such initial values for the stoppin power function I obtain a positive value
-@given(x=st.just(10),xc=st.just(10),A=st.just(100),sigma=st.just(0.5),tau=st.just(2), C1=st.just(5))
-def test_stopping_power(x,xc,A,sigma,tau, C1):
-    model= functions_definition.stopping_power(x,xc,A,sigma,tau,C1)
-    assert model>0
-
-
-
-
-#test if the output of the fitting of the stopping power are complete
-@given(A=st.just(100),sigma=st.just(0.5), tau=st.just(2), C1=st.just(5))
-def test_output_fitting_sp(A,sigma,tau, C1):
-
-    config = configparser.ConfigParser()
-    config.read('configuration.txt')
-
-
-
-
-    stopping_power = config.get('paths', 'stopping_power')
-    stopping_power_data_file=np.loadtxt(os.path.expanduser(stopping_power),comments='%')
-   
-    theorical_x, theorical_y = functions_definition.Fitting("Stopping power", functions_definition.stopping_power, stopping_power_data_file, A, sigma, tau, C1)
-
-    assert 'A'  in theorical_y.params
-    assert 'xc' in theorical_y.params
-    assert 'sigma' in theorical_y.params
-    assert 'tau' in theorical_y.params
-    assert 'C1' in theorical_y.params
-
-
-#test if the output of the fitting of the cross section are complete
-@given(A=st.just(10),sigma=st.just(0.5), tau=st.just(2),C1=st.just(5))
-def test_output_fitting_cs(A,sigma,tau,C1):
-
-    config = configparser.ConfigParser()
-    config.read('configuration.txt')
-
-
-
-
-    cross_section = config.get('paths', 'sezione_urto')
-
-    cross_section = config.get('paths', 'sezione_urto')
-    cross_section_data_file=np.loadtxt(os.path.expanduser(cross_section),comments='%')
+    x_data = np.linspace(0, 10, 100)
+    y_data = cross_section(x_data, xc=5, A=10, sigma=1, tau=2) 
+    data = np.column_stack((x_data, y_data))
     
-    theorical_x, theorical_y = functions_definition.Fitting("Name", functions_definition.cross_section, cross_section_data_file, A, sigma, tau, C1)
+    theorical_x, result = Fitting(cross_section, data, A=10, sigma=1, tau=2)
+    
+    params = result.params
+    
+    assert params['xc'].value > 0, "Parameter 'xc' should be positive"
+    assert params['A'].value > 0, "Parameter 'A' should be positive"
+    assert params['sigma'].value > 0, "Parameter 'sigma' should be positive"
+    assert params['tau'].value > 0, "Parameter 'tau' should be positive"
 
-    assert 'A'  in theorical_y.params
-    assert 'xc' in theorical_y.params
-    assert 'sigma' in theorical_y.params
-    assert 'tau' in theorical_y.params
-    assert 'C1' not in theorical_y.params
- 
+def test_fitting_sp_params_positive():
 
-
-
-
-
-
+    x_data = np.linspace(0, 10, 100)
+    y_data = stopping_power(x_data, xc=5, A=150, sigma=0.5, tau=2, C1=5) 
+    data = np.column_stack((x_data, y_data))
+    
+    theorical_x, result = Fitting(stopping_power, data, A=150, sigma=0.5, tau=2, C1=5)
+    
+    params = result.params
+    
+    assert params['xc'].value > 0, "Parameter 'xc' should be positive"
+    assert params['A'].value > 0, "Parameter 'A' should be positive"
+    assert params['sigma'].value > 0, "Parameter 'sigma' should be positive"
+    assert params['tau'].value > 0, "Parameter 'tau' should be positive"
